e2e104cf43f90b9af930717e1e808a5936becc0c
manuXD270516 <manueljss@live.com>
2018-09-21 15:47:37 -0400

    #1> diseño del login de la aplicacion movil Integracion de la API de Facebook para efecutar el login de la aplicacion

---
+++ app/src/main/java/com/ficct/reclamostopicos/reclamosscz/ViewLogin.java
@@ -0,0 +1,13 @@
+package com.ficct.reclamostopicos.reclamosscz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_login);
+    }
+}
